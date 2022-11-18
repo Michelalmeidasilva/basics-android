aed5d874aef0752ae79d5018797f1c1f69b142c4
Michel Silva <michelalmeidasilva@outlook.pt>
2022-11-18 11:15:06 -0300

    wip: added a initial form

---
+++ app/src/main/java/com/example/basicsandroid/ui/activity/ProductForm.kt
@@ -0,0 +1,8 @@
+package com.example.basicsandroid.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.basicsandroid.R
+
+class ProductForm : AppCompatActivity(R.layout.activity_product_form) {
+
+}
