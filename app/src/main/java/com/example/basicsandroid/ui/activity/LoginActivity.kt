02a67b0885c215ebfbbc7b6a6a84aa2f7c968a80
Michel Silva <michelalmeidasilva@outlook.pt>
2022-11-18 12:18:51 -0300

    wip: added a login screen

---
+++ app/src/main/java/com/example/basicsandroid/ui/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basicsandroid.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basicsandroid.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
