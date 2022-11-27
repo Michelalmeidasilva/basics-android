ba67214218048948989b59c298782ea0de564471
Michel Silva <michelalmeidasilva@outlook.pt>
2022-11-27 15:15:54 -0300

    refactor: added a new image component

--- app/src/main/java/com/example/basicsandroid/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/example/basicsandroid/ui/activity/ProductFormActivity.kt
@@ -1,7 +1,10 @@
 package com.example.basicsandroid.ui.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.basicsandroid.R
 import com.example.basicsandroid.dao.ProductsDao
@@ -12,6 +15,27 @@ import java.math.BigDecimal
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
     private lateinit var binding: ActivityProductFormBinding;
     private val productsDao = ProductsDao()
+
+    val REQUEST_IMAGE_GET = 1
+
+    fun selectImage() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_GET)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == Activity.RESULT_OK) {
+            val thumbnail: Bitmap? = data?.getParcelableExtra("data")
+            val fullPhotoUri: Uri? = data?.data
+
+        }
+    }
+
 
     private fun createProduct(): Product {
         val nameValue = binding.productFormName.text.toString()
@@ -28,6 +52,16 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
             productsDao.createProduct(newProduct)
             finish();
         }
+        binding.cameraAddedButton.setOnClickListener {
+
+            selectImage()
+        }
+
+        binding.photoAddedButton.setOnClickListener {
+
+            selectImage()
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +69,10 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
 
         binding = ActivityProductFormBinding.inflate(layoutInflater)
         val view = binding.root;
-        
+
         bindButton();
         setContentView(view)
     }
 
 }
+
