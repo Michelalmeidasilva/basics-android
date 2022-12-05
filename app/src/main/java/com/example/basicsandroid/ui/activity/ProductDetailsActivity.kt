48af30e25027bbbdc2b8e0dbc91651217f7e334e
Michel Silva <michelalmeidasilva@outlook.pt>
2022-12-05 13:12:54 -0300

    first commit

--- app/src/main/java/com/example/basicsandroid/ui/activity/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/basicsandroid/ui/activity/ProductDetailsActivity.kt
@@ -1,12 +1,12 @@
 package com.example.basicsandroid.ui.activity
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.basicsandroid.R
 import com.example.basicsandroid.databinding.ActivityProductDetailsBinding
 import com.example.basicsandroid.model.Product
-
 
 class ProductDetailsActivity : AppCompatActivity(R.layout.activity_product_details) {
     private lateinit var binding: ActivityProductDetailsBinding;
